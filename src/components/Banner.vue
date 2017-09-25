b506ae5547f86d3c3739c468095aa7269b4c60ae
Manohar Lakkoju <manoharvirat@gmail.com>
2017-09-26 04:39:00 +0530

    Moved Events into its own route

---
+++ src/components/Banner.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="banner">
+    <div class="container techno">
+      <div class="content">
+        <div id="large-header" class="large-header">
+          <canvas id="techno-canvas"></canvas>
+          <h1 class="main-title">TECHNO <span class="thin">SEARCH</span></h1>
+        </div>
+      </div>
+    </div><!-- /container -->
+  </div>
+</template>
+
+<script>
+//  import Tweenlite from ''
+  import BannerAnim from '../banner-anim';
+  export default {
+    name: 'Banner',
+    mounted() {
+      BannerAnim();
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  *, *:after, *:before { -webkit-box-sizing: border-box; box-sizing: border-box; }
+  .clearfix:before, .clearfix:after { content: ''; display: table; }
+  .clearfix:after { clear: both; }
+
+  .banner {
+    background: #fff;
+    color: #383a3c;
+    font-weight: 400;
+    font-size: 1em;
+    line-height: 1.25;
+    font-family: 'Raleway', Calibri, Arial, sans-serif;
+  }
+
+
+
+  /* Header */
+  .banner .container {
+    padding: 0;
+  }
+
+  .large-header {
+    position: relative;
+    width: 100%;
+    background: #333;
+    overflow: hidden;
+    background-size: cover;
+    background-position: center center;
+    z-index: 1;
+  }
+
+  .techno .large-header {
+    background-image: url('../assets/demo-1-bg.jpg');
+  }
+
+  .main-title {
+    position: absolute;
+    margin: 0;
+    padding: 0;
+    color: #f9f1e9;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate3d(-50%,-50%,0);
+    transform: translate3d(-50%,-50%,0);
+  }
+
+  .techno .main-title {
+    text-transform: uppercase;
+    font-size: 4.2em;
+    letter-spacing: 0.1em;
+  }
+  .main-title .thin {
+    font-weight: 200;
+  }
+
+  @media only screen and (max-width : 768px) {
+    .techno .main-title {
+      font-size: 3em;
+    }
+  }
+</style>
