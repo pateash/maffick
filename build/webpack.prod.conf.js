26902499d047ca9b2301fb6dbaa43c81f5a5c5dc
Manohar Lakkoju <manoharvirat@gmail.com>
2017-10-03 20:48:04 +0530

    added code for generating _redirects file for netlify

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -95,6 +95,11 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../config/netlify'),
+        to: config.build.assetsRoot,
+        ignore: ['.*']
       }
     ])
   ]
